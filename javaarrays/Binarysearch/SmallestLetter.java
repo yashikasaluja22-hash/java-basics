140eed52ad4c01cce067836c2644ede16fda5da2
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-08-17 21:56:16 +0530

    binary ka sawaal

---
+++ javaarrays/Binarysearch/SmallestLetter.java
@@ -0,0 +1,19 @@
+package Binarysearch;
+// This is leetcode ques --> smallest letter greater tha target
+//here we have used ceiling approach
+//need to return letter as answer instaed if index no.
+public class SmallestLetter {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(target < letters[mid]){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
