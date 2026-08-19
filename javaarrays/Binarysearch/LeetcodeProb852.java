20567e4249568b782824f29474a77966ad77257b
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-08-19 23:11:41 +0530

    kal college jaana hai aur meri awaaz ki aesi ki tesi ho rakhi hai

---
+++ javaarrays/Binarysearch/LeetcodeProb852.java
@@ -0,0 +1,22 @@
+package Binarysearch;
+//we dont have a target in this problem
+//only find the moutain index of array
+public class LeetcodeProb852 {
+    public static void main(String[] args) {
+       int[] arr = {0,1,3,5,7,4,3,2};
+       System.out.println(peakIndexInMountainArray(arr));
+    }
+    public static int peakIndexInMountainArray(int[] arr) {
+            int start = 0;
+            int end = arr.length - 1;
+            while(start < end){
+                int mid = start + (end - start)/2;
+                if(arr[mid] < arr[mid + 1]){
+                    start = mid + 1;
+                }else{
+                    end = mid;
+                }
+            }
+            return start;
+    }
+}
