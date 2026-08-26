7307e6bb2865502d4d413e8c8a912924ee660d03
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-08-26 20:28:20 +0530

    med prob

---
+++ javaarrays/Binarysearch/LeetcodeProb153.java
@@ -0,0 +1,20 @@
+package Binarysearch;
+
+public class LeetcodeProb153 {
+    class Solution {
+        public int findMin(int[] nums) {
+            int start = 0;
+            int end = nums.length - 1;
+            while(start < end){
+                int mid = start + (end - start)/2;
+                if(nums[mid] > nums[end]){
+                    //means minimum is somewhere right side
+                    start = mid + 1;
+                }else{
+                    end = mid;
+                }
+            }
+            return nums[start];
+        }
+    }
+}
