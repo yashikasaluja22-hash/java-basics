0ac99caa0f96ab823f45d48546ebe3b36d031da2
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-08-29 23:46:59 +0530

    binary search in 2d matrix

---
+++ javaarrays/Binarysearch/BinarySearchinMatrix.java
@@ -0,0 +1,66 @@
+package Binarysearch;
+
+public class BinarySearchinMatrix {
+    class Solution {
+        public int[] searchMatrix(int[][] matrix, int target) {
+            int rows = matrix.length;
+            int cols = matrix[0].length;//cautious maybe matrix is empty or target is not in matrix
+            if(rows == 1){
+                binarySearch(matrix, 0, 0, cols - 1, target]);
+            }
+
+            int rStart = 0;
+            int rEnd = rows - 1;
+            int cMid = (cols - 1)/2;
+            //run the loop till 2 rows are remaining
+            while(rStart < (rEnd - 1)){
+                int mid = rStart + (rEnd - rStart)/2;
+                if(matrix[mid][cMid] == target){
+                    return new int[] {mid, cMid};
+                }
+                if(matrix[mid][cMid] < target){
+                    rStart = mid;
+                }else{
+                    rEnd = mid;
+                }
+            }
+            //now we have 2 rows
+            //check whether the target is in the middle column of 2 rows
+            if(matrix[rStart][cMid] == target){
+                return new int[]{rStart, cMid};
+            }
+            if(matrix[rStart + 1][cMid] == target){
+                return new int[]{rStart + 1, cMid};
+            }
+            //if target is not found then
+            if(target <= matrix[rStart][cMid - 1]){
+                return binarySearch(matrix, rStart, 0, cMid - 1, target);
+            }
+        }
+    if(target >= matrix[rStart][cMid + 1] && target <= matrix[rStart][cols - 1]){
+            return binarySearch(matrix, rStart, cMid + 1, cols - 1, target);
+        }
+    if(target <= matrix[rStart + 1][cMid - 1]){
+            return binarySearch(matrix, rStart + 1, 0, cMid - 1, target);
+        }else{
+            return binarySearch(matrix, rStart + 1, cMid + 1, cols - 1, target);
+        }
+
+
+        //to apply binary search in a single row which is like an array
+        public static int[] binarySearch(int[] matrix, int row, int cStart, int cEnd, int target){
+            while(cStart <= cEnd){
+                int mid = cStart + (cEnd - cStart)/2;
+                if(matrix[row][mid] == target){
+                    return new int[] {row, mid};
+                }
+                if(matrix[row][mid] > target){
+                    cEnd = mid - 1;
+                }else{
+                    cStart = mid + 1;
+                }
+            }
+            return new int[]{-1, -1};
+        }
+    }
+}
