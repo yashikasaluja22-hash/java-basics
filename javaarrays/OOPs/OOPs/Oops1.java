2e71591ba454a8537b726a39ceecb63f899fba12
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-08-30 12:40:55 +0530

    oops

---
+++ javaarrays/OOPs/OOPs/Oops1.java
@@ -0,0 +1,11 @@
+public class Oops1 {
+    public void main(String[] args) {
+        Student student1 = new Student();
+        System.out.println(student1);
+    }
+    class Student{
+        int roll_no;
+        String name;
+        float marks;
+    }
+}
