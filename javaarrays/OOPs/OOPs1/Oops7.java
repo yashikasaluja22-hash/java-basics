9b496077502e9ea2787fed8a10388de31174e8cd
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-09-02 22:58:35 +0530

    hey bat

--- javaarrays/OOPs/OOPs1/Oops7.java
+++ javaarrays/OOPs/OOPs1/Oops7.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Oops7 {
+    public void main(String[] args){
+     Human a = new Human(22, "Sarah", 10000, true);
+     Human b = new Human(54, "harry", 2000, false);
+     System.out.println(a.population);
+     System.out.println(b.population);
+    }
+class Human{
+   int age;
+   String name;
+   int salary;
+   boolean married;
+   static long population;
+   Human(int age, String name, int salary, boolean married){
+       this.age = age;
+       this.name = name;
+       this.salary = salary;
+       this.married = married;
+       Human.population += 1;
+   }
+}
+
 }
