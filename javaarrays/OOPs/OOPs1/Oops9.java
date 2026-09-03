bc3bccc06fccb5f395cab602b0f36b7ad81263ff
yashikasaluja22-hash <yashikasaluja22@gmail.com>
2026-09-04 00:59:21 +0530

    yaar kal java ka first test hai

--- javaarrays/OOPs/OOPs1/Oops9.java
+++ javaarrays/OOPs/OOPs1/Oops9.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Oops9 {
+    //this is a demo for intialisation of static variables
+    //run the code and observe
+    static int a = 4;
+    static int b;
+
+    //this static method will run only once when first object is createdd i.e. when the class is loaded for first time
+    static {
+        System.out.println("i m Static block");
+        b = a * 5;
+    }
+
+    public static void main(String[] args){
+        Oops9 obj1 = new Oops9();
+        System.out.println(Oops9.a + " " + Oops9.b);
+
+        b += 3;
+        System.out.println(Oops9.a + " " + Oops9.b);
+
+        Oops9 obj2 = new Oops9();
+        System.out.println(Oops9.a + " " + Oops9.b);
+    }
+
 }
